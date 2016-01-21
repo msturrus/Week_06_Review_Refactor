1c46bd77ea0fceeaee962e2397402bd382fb8c7d
msturrus <msturrus@gmail.com>
2016-01-21 17:59:45 -0600

    lesson 11 lab changes
    lesson 11 lab changes

---
+++ 11_arrays/solution_student/carousel/js/main.js
@@ -0,0 +1,3 @@
+//Show first image of the array, mute previous button
+//on click of the "next" button hide current image of the array and display the next image of the array
+//on "next" change the image src to the next image in sequence
--- 11_arrays/solution_student/temp_converter_solution/js/main.js
+++ 11_arrays/solution_student/temp_converter_solution/js/main.js
@@ -1,17 +1,18 @@
 
+//When you click the F to C button, run a function
 $('#fahrenheit_to_celsius').on('click', function() {
-
+//defining temp variable, changing input from a string to a float
   var temp = parseFloat($('#temperature').val());
-
+//defining the converted temp as the output of the f to C conversion
   var convertedTemp = (temp - 32) * 5/9;
-
+//runs the "background change" function, detailed later
   backgroundChange(temp);
-
+//if the inputed character is not a number...
   // hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/isNaN
   if (isNaN(temp)) {
-
+//run the adderror function
     addError();
-
+//otherwise select the converted temp result and output it in a reader-friendly fashion
   } else {
 
     $('#result').html(temp + "&deg;F = " + convertedTemp + "&deg;C");
@@ -40,36 +41,36 @@ $('#celsius_to_fahrenheit').on('click', function() {
   }
 });
 
-
+//definition of the "background Change" function, which depends on the temp input
 function backgroundChange(temp) {
-
+//changes the value of the class attribute of the body, making it blank
   $('body').attr('class', '');
-
+//if the inputed temperature is < 50
   if (temp < 50) {
-
+//add the "cold" class to the body
     $('body').addClass('cold');
-
+//Otherwise, if the temperature is between 50 and 70
   } else if (temp >= 50 && temp <= 70) {
-
+//add the "mild" class to the body
     $('body').addClass('mild');
-
+//otherwise, if the temp is > 70
   } else if (temp > 70) {
-
+//add the "hot"  class to the body
     $('body').addClass('hot');
 
   }
 }
 
-
+//definition of addError function
 function addError() {
-
+//adds the "error" class to the "#temperature" id
   $('#temperature').addClass('error');
-
+//fades in the "error-message" class and prompts the user to "add a value"
   $('.error-message').fadeIn(500).html('Please enter a value.');
 
 }
 
-
+//This function clears out the results of the "adderror" function
 function clearError() {
 
   $('.error-message').fadeOut(500);
@@ -78,7 +79,7 @@ function clearError() {
 
 }
 
-
+//this watches for new user key input
 $('#temperature').on('keyup', function() {
 
   $('#result').html('');
